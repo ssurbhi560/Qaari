ad792b2091de3803548ba9d16d1f275bf2b867ce
Surbhi Sharma <ssurbhi560@gmail.com>
2020-06-07 00:40:18 +0530

    adding Registration Form

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 db = SQLAlchemy(app) #creating db object that representts database
 migrate = Migrate(app, db) #object represents migration engine
+login = LoginManager(app)
+login.login_view = 'login'
 
-from app import routes, models #midules will define structure of the databaase
+from app import routes, models #modules will define structure of the databaase
 
