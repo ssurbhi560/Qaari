b9a4a6f0cb9680e1a9cf5f3206664152358fc710
Surbhi Sharma <ssurbhi560@gmail.com>
2021-05-06 22:26:25 +0530

    add dockerfile

--- app/main/routes.py
+++ app/main/routes.py
@@ -95,14 +95,14 @@ def follow(username):
     if form.validate_on_submit():
         user = User.query.filter_by(username=username).first()
         if user is None:
-            flash('User %(username)s not found.', username=username)
+            flash('User {} not found.'.format(username))
             return redirect(url_for('main.index'))
         if user == current_user:
             flash('You cannot follow yourself!')
             return redirect(url_for('main.user', username=username))
         current_user.follow(user)
         db.session.commit()
-        flash('You are following %(username)s!', username=username)
+        flash('You are following {} !'.format(username))
         return redirect(url_for('main.user', username=username))
     else:
         return redirect(url_for('main.index'))
@@ -115,14 +115,14 @@ def unfollow(username):
     if form.validate_on_submit():
         user = User.query.filter_by(username=username).first()
         if user is None:
-            flash('User %(username)s not found.', username=username)
+            flash('User {} not found.'.format(username))
             return redirect(url_for('main.index'))
         if user == current_user:
             flash('You cannot unfollow yourself!')
             return redirect(url_for('main.user', username=username))
         current_user.unfollow(user)
         db.session.commit()
-        flash('You are not following %(username)s.', username=username)
+        flash('You are not following {}.'.format(username))
         return redirect(url_for('main.user', username=username))
     else:
         return redirect(url_for('main.index'))
