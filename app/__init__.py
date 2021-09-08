e297ac8c3ca33c3a253f5b09d4daa830eca69c56
Surbhi Sharma <ssurbhi560@gmail.com>
2021-09-08 15:21:08 +0530

    remove unused imports

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask, request, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from config import Config
@@ -15,7 +14,6 @@ migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = ('Please log in to access this page.')
-mail = Mail()
 bootstrap = Bootstrap()
 moment = Moment()
 
@@ -27,7 +25,6 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
-    mail.init_app(app)
     bootstrap.init_app(app)
     moment.init_app(app)
 
